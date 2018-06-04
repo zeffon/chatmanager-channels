22cb2591d8839f1ba4faff85c2d85b909cedda1e
zeffon <zephoony@gmail.com>
2018-06-04 17:26:50 +0800

    first stage

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,13 @@
+'''
+chat view
+'''
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    '''
+    index view
+    '''
+    return render(request, 'chat/index.html', {})
--- chatmanager/settings.py
+++ chatmanager/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+ASGI_APPLICATION = 'chatmanager.routing.application'
