8263108459d5d68af92b1c023dd731915b0b1556
zeffon <zephoony@gmail.com>
2018-06-04 22:49:14 +0800

    staging2

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,8 @@
 chat view
 '''
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
 
 # Create your views here.
 
@@ -11,3 +13,10 @@ def index(request):
     index view
     '''
     return render(request, 'chat/index.html', {})
+
+def room(request, room_name):
+    return render(
+        request,
+        'chat/room.html',
+        {'room_name_json':  mark_safe(json.dumps(room_name))}
+    )
